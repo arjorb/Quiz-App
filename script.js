0996c458340447f20a58728e20beb16cd543848a
John U <arjorb@gmail.com>
2022-03-14 11:55:07 +0200

    quizz app done

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const quizDate = [
         a: 'Trump Donard',
         b: 'Barack Obama',
         c: 'Joe Baiden',
+        d: 'John U',
         correct:'c'
     },
     {
@@ -43,3 +44,71 @@ const quizDate = [
 
     }
 ]
+
+const question_text = document.getElementById('question_text')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+
+const answerRadio = document.querySelectorAll('.answer') 
+
+const submitBtn = document.getElementById('submit');
+
+const quizEl = document.getElementById('quiz-el');
+
+let currentQuestion = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselect();
+    const currentQuestionData = quizDate[currentQuestion];
+
+    question_text.innerHTML = currentQuestionData.question;
+    a_text.innerHTML = currentQuestionData.a;
+    b_text.innerHTML = currentQuestionData.b;
+    c_text.innerHTML = currentQuestionData.c;
+    d_text.innerHTML = currentQuestionData.d;
+    
+}
+
+function getSelected(){
+    let result;
+    answerRadio.forEach(answer =>{
+        if(answer.checked){
+            result = answer.id;
+        }  
+    })
+    return result;
+}
+
+function deselect(){
+    answerRadio.forEach(answer =>{
+        answer.checked = false;
+})
+};
+submitBtn.addEventListener('click',() =>{
+    const answer = getSelected();
+
+    if(answer){
+    if(answer === quizDate[currentQuestion].correct){
+        score++
+    }
+        currentQuestion++
+        if(currentQuestion < quizDate.length){
+            loadQuiz()
+        }else{
+            if(score >=3){
+                quizEl.innerHTML =`<h3>You have scored at ${score} out 5 💃🏾 🍾 </h3><button onclick='window.location.reload()'>Reload</button>`;
+            }else{
+                quizEl.innerHTML =`<h3>You have scored at ${score} out 5 🤦🏾‍♂️ 🤯</h3> <button onclick='window.location.reload()'>Reload</button>`;
+            }
+
+        }  
+        }else{
+        console.log('Nothing selected')
+        }   
+})
+
