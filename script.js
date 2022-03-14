0409985da51af1435f81458463da6b33ca12fcb8
John U <arjorb@gmail.com>
2022-03-14 10:26:32 +0200

    Style finished

---
+++ script.js
@@ -0,0 +1,45 @@
+const quizDate = [
+    {
+        question:'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct:'c'
+
+    },
+    {
+        question:'What is the best Programming language?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'Javascript',
+        correct:'d'
+
+    },
+    {
+        question:'Who is the current president of USA?',
+        a: 'Trump Donard',
+        b: 'Barack Obama',
+        c: 'Joe Baiden',
+        correct:'c'
+    },
+    {
+        question:'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascanding Style Sheet',
+        c: 'Javascript Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborg',
+        correct:'a'
+
+    },
+    {
+        question:'What year was Javascript launched?',
+        a: '1997',
+        b: '1982',
+        c: '1995',
+        d: 'none of the above',
+        correct:'c'
+
+    }
+]
